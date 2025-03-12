dedbf190db9dd99addfe6e152051ce5d2c7c5240
Maria Polak <mpolak23@student.aau.dk>
2025-03-12 17:48:22 +0100

    Clean Up

--- dsp/source.py
+++ dsp/source.py
@@ -1,6 +1,4 @@
-import librosa
 import numpy as np
-import soundfile as sf
 
 from ps.pv_dr.common.track import Track, TrackInfo
 
@@ -18,25 +16,3 @@ class AudioSource:
 
     def get_track(self) -> Track:
         return self.track
-
-
-# class WavFileReader(AudioSource):
-#     """Reads all data from a WAVE file and stores them into a Track."""
-#     def __init__(self, info: TrackInfo, pitch_shift=True):
-#         in_file = get_resources_root_test_data_path(info.name + ".wav")
-
-#         track = Track()
-#         track.info = info
-#         data, track.info.sample_rate = sf.read(in_file, dtype='float32')
-#         if len(np.shape(data)) > 1:
-#             track.base = np.zeros(len(data))
-#             channels = len(data[0])
-#             for i in range(len(data)):
-#                 track.base[i] = sum(data[i])
-#             track.base /= channels
-#         else:
-#             track.base = data
-
-#         track.info.setup()
-        
-#         super().__init__(track)
